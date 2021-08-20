80fb13417ae8a45bf76968a41d83f9a414e7f5dc
Reynadi Andreas Priesley <yerpa531@gmail.com>
2021-08-20 19:43:30 +0700

    Fix cache problem

--- src/client.ts
+++ src/client.ts
@@ -7,9 +7,7 @@ import { Command } from './types/command';
 import { Event } from './types/event';
 import { Autoresponse } from './types/autoresponse';
 import mongoose from 'mongoose';
-import prefixModel from './models/prefixModel';
-import autoresponseModel from './models/autorepsonseModel';
-import papdongModel from './models/papdongModel';
+
 
 class Bot extends Client {
     public globPromise = promisify(Glob);
@@ -34,105 +32,6 @@ class Bot extends Client {
             .then(e => this.logger.success('Connected to database'))
             .catch(err => this.logger.error(err))
 
-
-        prefixModel.find({}, (err, docs) => {
-            this.listprefix = {}
-            if(err) {
-                console.error('Erorr fetching prefix config')
-            }
-            docs.forEach(a => {
-                // @ts-ignore
-                this.listprefix[a.guildId] = a.prefix
-            })
-        })
-
-
-        prefixModel.watch().on('change', change => {
-            if(change.operationType == 'insert') {
-                this.listprefix[change.fullDocument.guildId] = change.fullDocument.prefix;
-            }
-            if(change.operationType == 'update') {
-                prefixModel.findById(change.documentKey['_id'], (err, docs) => {
-                    if(err) {
-                        console.error(err)
-                    }
-                    this.listprefix[docs.guildId] = docs.prefix
-                })
-            }
-            if(change.operationType == 'delete') {
-                prefixModel.find({}, (err, docs) => {
-                    this.listprefix = {}
-                    if(err) {
-                        console.error('Erorr fetching prefix config')
-                    }
-                    docs.forEach(a => {
-                        // @ts-ignore
-                        this.listprefix[a.guildId] = a.prefix
-                    })
-                })
-            }
-        })
-
-        autoresponseModel.watch().on('change', change => {
-            if(change.operationType == 'delete' || change.operationType == 'update' || change.operationType == 'insert') {             
-                autoresponseModel.find({}, (err, docs: Autoresponse[]) => {
-                    if(err) {
-                        console.error(err)
-                    }
-
-                    this.guilds.cache.forEach(x => {
-                        this.autoresponseobject[x.id] = []
-                    })
-
-                    docs.forEach(x => {
-                        const tempdata = {}
-                        tempdata[x.trigger] = x.response
-                        this.autoresponseobject[x.guildId] = {...this.autoresponseobject[x.guildId], ...tempdata}
-                    })
-                })
-            }
-        })
-
-        autoresponseModel.find({}, (err, docs: Autoresponse[]) => {
-            if(err) {
-                console.error(err)
-            }
-
-            this.guilds.cache.forEach(x => {
-                this.autoresponseobject[x.id] = []
-            })
-
-            docs.forEach(x => {
-                const tempdata = {}
-                tempdata[x.trigger] = x.response
-                this.autoresponseobject[x.guildId] = {...this.autoresponseobject[x.guildId], ...tempdata}
-             })
-        })
-
-        papdongModel.find({}, (err, docs: string[]) => {
-            if(err) {
-                console.error(err)
-            }
-            this.papdong = {}
-            this.guilds.cache.forEach(x => {
-                // @ts-ignore
-                this.papdong[x.id] = docs.filter(a => a.guildId == x.id)
-            })
-        })
-        
-        papdongModel.watch().on('change', change => {
-            papdongModel.find({}, (err, docs: string[]) => {
-                if(err) {
-                    console.error(err)
-                }
-                
-                this.papdong = {}
-                this.guilds.cache.forEach(x => {
-                    // @ts-ignore
-                    this.papdong[x.id] = docs.filter(a => a.guildId == x.id)
-                })
-            })
-        })
 
         const commandFiles: string[] = await this.globPromise(`${__dirname}/commands/**/*{.ts,.js}`);
         console.log('=== COMMANDS ===')
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,110 @@
 import { RunFunction } from '../types/event';
+import prefixModel from '../models/prefixModel';
+import autoresponseModel from '../models/autorepsonseModel';
+import papdongModel from '../models/papdongModel';
+import { Autoresponse } from '../types/autoresponse';
 
 const name: string = 'ready';
 const run: RunFunction = async(client) => {
+    prefixModel.find({}, (err, docs) => {
+        client.listprefix = {}
+        if (err) {
+            console.error('Erorr fetching prefix config')
+        }
+        docs.forEach(a => {
+            // @ts-ignore
+            client.listprefix[a.guildId] = a.prefix
+        })
+    })
+
+
+    prefixModel.watch().on('change', change => {
+        if (change.operationType == 'insert') {
+            client.listprefix[change.fullDocument.guildId] = change.fullDocument.prefix;
+        }
+        if (change.operationType == 'update') {
+            prefixModel.findById(change.documentKey['_id'], (err, docs) => {
+                if (err) {
+                    console.error(err)
+                }
+                client.listprefix[docs.guildId] = docs.prefix
+            })
+        }
+        if (change.operationType == 'delete') {
+            prefixModel.find({}, (err, docs) => {
+                client.listprefix = {}
+                if (err) {
+                    console.error('Erorr fetching prefix config')
+                }
+                docs.forEach(a => {
+                    // @ts-ignore
+                    client.listprefix[a.guildId] = a.prefix
+                })
+            })
+        }
+    })
+
+    autoresponseModel.watch().on('change', change => {
+        if (change.operationType == 'delete' || change.operationType == 'update' || change.operationType == 'insert') {
+            autoresponseModel.find({}, (err, docs: Autoresponse[]) => {
+                if (err) {
+                    console.error(err)
+                }
+
+                client.guilds.cache.forEach(x => {
+                    client.autoresponseobject[x.id] = []
+                })
+
+                docs.forEach(x => {
+                    const tempdata = {}
+                    tempdata[x.trigger] = x.response
+                    client.autoresponseobject[x.guildId] = { ...client.autoresponseobject[x.guildId], ...tempdata }
+                })
+            })
+        }
+    })
+
+    autoresponseModel.find({}, (err, docs: Autoresponse[]) => {
+        if (err) {
+            console.error(err)
+        }
+
+        client.guilds.cache.forEach(x => {
+            client.autoresponseobject[x.id] = []
+        })
+
+        docs.forEach(x => {
+            const tempdata = {}
+            tempdata[x.trigger] = x.response
+            client.autoresponseobject[x.guildId] = { ...client.autoresponseobject[x.guildId], ...tempdata }
+        })
+    })
+
+    papdongModel.find({}, (err, docs: string[]) => {
+        if (err) {
+            console.error(err)
+        }
+        client.papdong = {}
+        client.guilds.cache.forEach(x => {
+            // @ts-ignore
+            client.papdong[x.id] = docs.filter(a => a.guildId == x.id)
+        })
+    })
+
+    papdongModel.watch().on('change', change => {
+        papdongModel.find({}, (err, docs: string[]) => {
+            if (err) {
+                console.error(err)
+            }
+
+            client.papdong = {}
+            client.guilds.cache.forEach(x => {
+                // @ts-ignore
+                client.papdong[x.id] = docs.filter(a => a.guildId == x.id)
+            })
+        })
+    })
+
     client.logger.success(`Logged in as ${client.user.tag}`);
 } 
 
